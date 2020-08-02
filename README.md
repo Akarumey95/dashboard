bc78c3f7908d46ffb84f475a672f3c9c1ef506c9
Yaroslav <yroslav005@gmail.com>
2020-08-02 09:42:44 +0300

    Registr all in my custom service provider class

--- README.md
+++ README.md
@@ -1,26 +1,19 @@
 ## Dashboard
 
-[![Stable version](https://img.shields.io/badge/version-v1.0.3-green)](https://packagist.org/packages/akarumey95/dashboard)
+[![Stable version](https://img.shields.io/badge/version-v1.0.4-green)](https://packagist.org/packages/akarumey95/dashboard)
 
 
 1)For install `composer require akarumey95/dashboard`
 
 2)Open your `config/app.php` and add the following to the `providers` array:
 ```php
-Dashboard\Providers\ComposerServiceProvider::class,
 Dashboard\Providers\DashboardServiceProvider::class,
 ```
-3)And add to the `aliases` array:
+and to the `aliases` array:
 ```php
 'Dashboard' => Dashboard\Facades\Dashboard::class,
 ```
-4)Open `app/Console/Kernel.php` and add to `$commands` array:
-```php
-GenerateDashboardControllerCommand::class,
-GenerateDashboardViewsCommand::class,
-InstallDashboardCommand::class,
-```
-5)Run in console
+3)Run in console
 ```shell script
 php artisan dashboard:install
 ```
--- src/Dashboard/Providers/DashboardServiceProvider.php
+++ src/Dashboard/Providers/DashboardServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace Akarumey95\Dashboard\Providers;
 
+use Akarumey95\Dashboard\Commands\GenerateDashboardControllerCommand;
+use Akarumey95\Dashboard\Commands\GenerateDashboardViewsCommand;
+use Akarumey95\Dashboard\Commands\InstallDashboardCommand;
 use Akarumey95\Dashboard\Dashboard;
 use Akarumey95\Dashboard\DashboardRouteMethods;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class DashboardServiceProvider extends ServiceProvider
@@ -17,6 +21,12 @@ class DashboardServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->commands([
+            GenerateDashboardControllerCommand::class,
+            GenerateDashboardViewsCommand::class,
+            InstallDashboardCommand::class,
+        ]);
+
         App::bind('dashboard',function() {
             return new Dashboard;
         });
@@ -30,5 +40,30 @@ class DashboardServiceProvider extends ServiceProvider
     public function boot()
     {
         Route::mixin(new DashboardRouteMethods);
+
+        $controllers = config('dashboard.controllers');
+
+        if($controllers){
+            foreach ($controllers as $controller){
+                View::composer([
+                    $controller['view'] . '.index',
+                    $controller['view'] . '.show',
+                    $controller['view'] . '.form',
+                ], function($view) use ($controller) {
+                    $view->with([
+                        'homePage' => $controller['homePage'],
+                        'modelName' => $controller['modelName'],
+                    ]);
+                });
+
+                View::composer([
+                    $controller['view'] . '.form',
+                ], function($view) use ($controller) {
+                    $view->with([
+                        'fillable' => (new $controller['model'])->getFillable(),
+                    ]);
+                });
+            }
+        }
     }
 }
